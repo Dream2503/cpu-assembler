717aed4a95ff10b3cb7cc359bbe6bc6589a9d37f
Dream <swapnarajmohanty2503@gmail.com>
2025-09-30 13:18:25 +0530

    Add documentation for CPU architecture constants
    Added a comment block describing the purpose of architecture constants and type aliases in cpu.hpp, clarifying separation of concerns and usage.

--- cpu.hpp
+++ cpu.hpp
@@ -1,5 +1,12 @@
 #pragma once
 
+/*
+CPU Architecture Constants
+
+- Defines architecture width and type aliases.
+- Follows separation of concerns (SOC): only architecture and type info here.
+*/
+
 using uint8_t = unsigned char;
 using int8_t = signed char;
 constexpr uint8_t ARCHITECTURE = 16;
